2a5067b2945931a72e3f5086d55bccfbc91b87e7
tecboyj <jc515081@gmail.com>
2023-10-02 10:03:46 -0400

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 
 void main() {
@@ -120,6 +121,27 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+*/
+
+import 'package:flet/flet.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  await setupDesktop();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flet Flutter Demo',
+      home: FletApp(pageUrl: "http://localhost:8550", assetsDir: 'assets')
     );
   }
 }
